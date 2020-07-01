b26a7640c17938ee2d149b31a6db1c9349989c5e
RouJones1990 <will-anthony@hotmail.com>
2020-07-01 20:07:33 +0100

    -Bear enemy added -Game breaking bug, needs fix

---
+++ core/src/com/jga/jumper/controllers/EnemyController.java
@@ -0,0 +1,20 @@
+package com.jga.jumper.controllers;
+
+import com.badlogic.gdx.utils.Array;
+import com.jga.jumper.entity.EnemyBase;
+import com.jga.jumper.entity.Monster;
+import com.jga.jumper.entity.Slug;
+
+public interface EnemyController<T extends EnemyBase> {
+
+    void update(float delta);
+    void enemySpawnLogic(T enemy);
+    void enemyWalkLogic(T enemy, float delta);
+    void enemyAttackLogic(T enemy, float delta);
+    void enemyDyingLogic(T enemy, float delta);
+    void enemyDeathLogic(T enemy);
+    boolean isEnemyNearby(float angle);
+    void checkMonsterCollision(T enemy, Monster monster);
+    void checkEnemyCollision(EnemyBase thisEnemy, Array<T> otherEnemies);
+
+}
